870113cba645f2a909b8e173e192d3f9eb2a145f
Kenny Wibowo <kewibowo@ucsd.edu>
2016-07-30 21:50:10 -0700

    Formatting, tabs -> spaces

--- js/ConnectFall.js
+++ js/ConnectFall.js
@@ -2,34 +2,34 @@ var ConnectFall = angular.module('ConnectFall', []);
 
 
 ConnectFall.controller('Game', ['$scope', function($scope) {
-	var width = 7;
-	var height = 6;
+    var width = 7;
+    var height = 6;
 
-	function initBoard(width, height) {
-		var board = new Array(height);
+    function initBoard(width, height) {
+        var board = new Array(height);
 
-		for( i = 0; i < height; i++ ) {
-			board[i] = new Array(width)
-			for( j = 0; j < width; j++ ) {
-				board[i][j] = "empty"; // TODO maybe represent as integer value instead?
-			}
-		}
+        for( i = 0; i < height; i++ ) {
+            board[i] = new Array(width)
+            for( j = 0; j < width; j++ ) {
+                board[i][j] = "empty"; // TODO maybe represent as integer value instead?
+            }
+        }
 
-		return board
-	}
+        return board
+    }
 
-	$scope.status_enum = ["empty", "red", "blue"];
+    $scope.status_enum = ["empty", "red", "blue"];
     $scope.board = initBoard(width, height);
    
 }])
 .directive('connectFall', function() {
 
-	return {
-		templateUrl: "templates/board.html"
-	}
+    return {
+        templateUrl: "templates/board.html"
+    }
 })
 .directive('cfTile', function() {
-	var status = "empty"; // TODO parameterize this, maybe as a integer value?
+    var status = "empty"; // TODO parameterize this, maybe as a integer value?
     return {
         template: "<img src='res/tile_" + status + ".png'>" 
     };
