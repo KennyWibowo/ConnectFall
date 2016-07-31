4b25807b8c7d2d9dd5a12e972a9f72af28a97576
Kenny Wibowo <kewibowo@ucsd.edu>
2016-07-30 19:12:47 -0700

    Simple directive definition

--- js/ConnectFall.js
+++ js/ConnectFall.js
@@ -3,3 +3,8 @@ var ConnectFall = angular.module('ConnectFall', []);
 ConnectFall.controller('game_main', ['$scope', function($scope) {
     $scope.name = "ConnectFall"
 }])
+.directive('cfTile', function() {
+    return {
+        template: "<img src='res/tile_empty.png'></img>" 
+    };
+});
